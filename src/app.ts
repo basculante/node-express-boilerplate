9b1ed426c1c61cea8f8748661a184ce5aba10d96
basculante <basculantetank@gmail.com>
2022-11-04 15:35:37 -0500

    Add cors and parse body json.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
 import Morgan from "./middleware/morgan";
 
 const port = process.env.PORT || 4000;
 const app = express();
 
+app.use(express.json());
+app.use(cors());
 app.use(Morgan);
 
 app.get("/", (req, res) => {
